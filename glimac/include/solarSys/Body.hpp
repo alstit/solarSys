a60bad5d82579cd81035b790e714528c323dca35
alstit <alexyls78@gmail.com>
2020-12-09 05:54:11 +0100

    Add files via upload

--- glimac/include/solarSys/Body.hpp
+++ glimac/include/solarSys/Body.hpp
@@ -1,13 +1,21 @@
-
+#include <glimac/SDLWindowManager.hpp>
 #include <glm/glm.hpp>
+#include <queue> 
+
+float UNITEASTRONOMIQUE = 149.6e9;
+
 
 using namespace glm;
+
+
 class Body
 {    public:
         float scale;
         float masse;
         vec3 position;
         vec3 initSpeed;
+        vec3 positionScreen ;
+        std::queue<glm::vec3> previousPos;
 
         Body( float scale,float masse,vec3 position,vec3 initSpeed)
         {
@@ -15,13 +23,54 @@ class Body
             this->masse = masse;
             this->position = position;
             this->initSpeed = initSpeed;
+
         }
 
-        viewMatrix(glm::mat4 MVMatrix)
+        glm::mat4 viewMatrix( SDLWindowManager* windowManager,glm::mat4 MVMatrix)
         {
-            MVMatrix = translate(MVMatrix,earth.position);
-            MVMatrix = rotate(MVMatrix, windowManager.getTime(), glm::vec3(0,1,0));
-            MVMatrix = glm::scale(MVMatrix, (float)earth.scale*glm::vec3(1, 1, 1));
+            MVMatrix = translate(MVMatrix,this->position/UNITEASTRONOMIQUE);
+            MVMatrix = rotate(MVMatrix, windowManager->getTime(), glm::vec3(0,1,0));
+            MVMatrix = glm::scale(MVMatrix, this->scale/UNITEASTRONOMIQUE*glm::vec3(1, 1, 1));
+
+            return MVMatrix;
         }
+
+        void update(float dt, std::vector<Body> bodyVect,glm::vec3 aforce);
+
+
 
 };
+
+glm::vec3 gforce(Body p1,Body p2){
+    // Calculate the gravitational force exerted on p1 by p2.
+    double G = 6.67e-11 ; 
+    // Calculate distance vector between p1 and p2.
+    float r_mag = glm::length(p1.position-p2.position);
+    glm::vec3 r_hat = glm::normalize(p1.position-p2.position);
+    // Calculate force magnitude.
+    float force_mag = G*p1.masse*p2.masse/(r_mag*r_mag) ;///1000 = masse
+    //float force_mag = 10000000;
+    //Calculate force vector.
+    glm::vec3 force_vec = -force_mag*r_hat;
+    
+    return force_vec;
+};
+
+
+void Body::update(float dt, std::vector<Body> bodies,glm::vec3 aforce)
+{
+    glm::vec3 Force = aforce;
+
+    for(int i = 0; i<bodies.size();i++)
+    {
+        Force += gforce(*this,bodies[0]);
+    }
+
+    this->initSpeed+=Force/(float)(bodies.size()+1)/this->masse*(float)dt;
+    this->position+=this->initSpeed*(float)dt;
+
+    this->previousPos.push(this->position);
+    if(this->previousPos.size()>10){this->previousPos.pop();}
+
+
+}
--- glimac/include/solarSys/Engine.hpp
+++ glimac/include/solarSys/Engine.hpp
@@ -1,13 +1,16 @@
 #include <glimac/FilePath.hpp>
 #include <glimac/Program.hpp>
 #include <glimac/common.hpp>
+#include <string>
 
 using namespace glimac;
-class Engine
+
+class Object3D 
 {
     public:
-    Program m_Program;
-
+        Program program;
+        GLuint *vbo = new GLuint();
+        GLuint *vao = new GLuint();
         GLint uMVPMatrix;
         GLint uMVMatrix;
         GLint uNormalMatrix;
@@ -17,17 +20,137 @@ class Engine
         GLuint uLightDir_vs;
         GLuint uLightIntensity;
 
-        Engine(const FilePath& applicationPath):
-        m_Program(loadProgram(applicationPath.dirPath() + "shaders/3D.vs.glsl",
-                                applicationPath.dirPath() + "shaders/directionallight.fs.glsl")) 
-                                    {
-                                        uMVPMatrix = glGetUniformLocation(m_Program.getGLId(), "uMVPMatrix");
-                                        uMVMatrix = glGetUniformLocation(m_Program.getGLId(), "uMVMatrix");
-                                        uNormalMatrix = glGetUniformLocation(m_Program.getGLId(), "uNormalMatrix");
-                                        uKd = glGetUniformLocation(m_Program.getGLId(), "uKd");
-                                        uKs = glGetUniformLocation(m_Program.getGLId(),"uKs");
-                                        uShininess = glGetUniformLocation(m_Program.getGLId(),"uShininess");
-                                        uLightDir_vs = glGetUniformLocation(m_Program.getGLId(),"uLightDir_vs");
-                                        uLightIntensity = glGetUniformLocation(m_Program.getGLId(),"uLightIntensity"); 
-                                    }
+        Object3D(){};
+
+
+        void load(const FilePath& applicationPath,std::string fragShader, std::string vertexShader) {
+            vertexShader = "shaders/" + vertexShader +".vs.glsl";
+            fragShader="shaders/" + fragShader+".fs.glsl";
+            this->program = loadProgram(applicationPath.dirPath() + vertexShader , applicationPath.dirPath() + fragShader) ;
+        };
+
+        void getUniforms()
+        {
+            this->program.use();
+            this->uMVPMatrix = glGetUniformLocation(this->program.getGLId(), "uMVPMatrix");
+            this->uMVMatrix = glGetUniformLocation(this->program.getGLId(), "uMVMatrix");
+            this->uNormalMatrix = glGetUniformLocation(this->program.getGLId(), "uNormalMatrix");
+            this->uKd = glGetUniformLocation(this->program.getGLId(), "uKd");
+            this->uKs = glGetUniformLocation(this->program.getGLId(),"uKs");
+            this->uShininess = glGetUniformLocation(this->program.getGLId(),"uShininess");
+            this->uLightDir_vs = glGetUniformLocation(this->program.getGLId(),"uLightDir_vs");
+            this->uLightIntensity = glGetUniformLocation(this->program.getGLId(),"uLightIntensity"); 
+        };
+
+
+};
+
+class Engine
+{
+    public:
+    Sphere asphere = Sphere(10, 64, 64);// defini par le rayon 
+    Object3D planetShaders;
+    Object3D trailShaders;
+
+
+
+
+
+    
+
+    ~Engine()
+    {
+        glDeleteBuffers(1,this->planetShaders.vbo);
+        glDeleteVertexArrays(1,this->trailShaders.vao);
+    }
+
+
+
+    Engine(const FilePath& applicationPath){
+        this->planetShaders.load(applicationPath,"directionallight","3D");
+
+        //this->trailShaders.load(applicationPath,"directionallight","3D");
+
+        this->openglBindBuffShpere();
+
+
+    };
+        
+
+    void openglBindBuffShpere()
+    {
+        this->planetShaders.program.use();
+
+        planetShaders.getUniforms();
+
+        glGenBuffers(1,this->planetShaders.vbo);
+        glBindBuffer(GL_ARRAY_BUFFER,*(this->planetShaders.vbo));
+        glBufferData(GL_ARRAY_BUFFER,this->asphere.getVertexCount()*sizeof(Sphere),this->asphere.getDataPointer(),GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER,0);
+
+        glGenVertexArrays(1,this->planetShaders.vao);
+        glBindVertexArray(*(this->planetShaders.vao));
+        glBindBuffer(GL_ARRAY_BUFFER,*(this->planetShaders.vbo));
+
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(ShapeVertex),(void*)offsetof(ShapeVertex,position));
+        glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,sizeof(ShapeVertex),(void*)offsetof(ShapeVertex,normal));
+        glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,sizeof(ShapeVertex),(void*)offsetof(ShapeVertex,texCoords));
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER,0);
+    };
+
+
+
+
+/*
+    void openglBindBuffTrail()
+    {
+        this->trailShaders.program.use();
+
+        glGenBuffers(1,this->trailShaders.vbo);
+        glBindBuffer(GL_ARRAY_BUFFER,*(this->trailShaders.vbo));
+        glBufferData(GL_ARRAY_BUFFER,this->asphere.getVertexCount()*sizeof(Sphere),this->asphere.getDataPointer(),GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER,0);
+
+        glGenVertexArrays(1,this->trailShaders.vao);
+        glBindVertexArray(*(this->trailShaders.vao));
+        glBindBuffer(GL_ARRAY_BUFFER,*(this->trailShaders.vbo));
+
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(ShapeVertex),(void*)offsetof(ShapeVertex,position));
+        glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,sizeof(ShapeVertex),(void*)offsetof(ShapeVertex,normal));
+        glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,sizeof(ShapeVertex),(void*)offsetof(ShapeVertex,texCoords));
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER,0);
+    };*/
+
+    void renderPlanet(glm::mat4 MVMatrix,glm::mat4 ProjMatrix, glm::mat4 NormalMatrix)
+    {
+        this->planetShaders.program.use();
+
+            glUniformMatrix4fv( 	this->planetShaders.uMVMatrix,
+                                        1,
+                                        GL_FALSE,
+                                        glm::value_ptr(MVMatrix));
+            glUniformMatrix4fv( 	this->planetShaders.uMVPMatrix,
+                                        1,
+                                        GL_FALSE,
+                                        glm::value_ptr(ProjMatrix*MVMatrix));
+            glUniformMatrix4fv( 	this->planetShaders.uNormalMatrix,
+                                        1,
+                                        GL_FALSE,
+                                        glm::value_ptr(NormalMatrix));                                                  
+
+            glBindVertexArray(*(this->planetShaders.vao));
+            glDrawArrays(GL_TRIANGLES,0,this->asphere.getVertexCount());
+            glBindVertexArray(0);
+    };
+
 };
