14543f1cbd2da191a55369b4d19fe2bf6020f90c
alstit <alexyls78@gmail.com>
2020-12-10 05:15:47 +0100

    add camera strategy pattern

--- moon.cpp
+++ moon.cpp
@@ -8,8 +8,7 @@
 #include <glimac/Program.hpp>
 #include <glimac/common.hpp>
 #include <glimac/Sphere.hpp>
-#include <glimac/TrackballCamera.hpp>
-#include <glimac/FreeflyCamera.hpp>
+#include <glimac/Camera.hpp>
 #include <math.h>
 #include <solarSys/Engine.hpp>
 #include <solarSys/Body.hpp>
@@ -18,12 +17,20 @@
 #include <SDL/SDL.h>
 #include <stdlib.h> 
 #include <solarSys/const.hpp>
+#include <typeinfo>
+#include <memory>
 
 
 using namespace glimac;
 using namespace std;
 
 
+
+
+template<typename Base, typename T>
+inline bool instanceof(const T*) {
+   return is_base_of<Base, T>::value;
+};
 
 
 int main(int argv,char** argc) {
@@ -48,35 +55,18 @@ int main(int argv,char** argc) {
 
     
 
-    FreeflyCamera freeCamera = FreeflyCamera();
-    TrackballCamera ballCamera = TrackballCamera();
-
-
-    
-
-
-    
-
-
-
-
-
+    Camera camera(std::unique_ptr<FreeflyCamera>(new FreeflyCamera()));
+    //camera = Camera(std::unique_ptr<TrackballCamera>(new TrackballCamera()));
 
     glEnable(GL_DEPTH_TEST);
     glm::mat4 ProjMatrix,MVMatrix,NormalMatrix;
+    
     float fov = 70;
     ProjMatrix = glm::perspective(glm::radians(fov),4.f/3.f,(float)0.00000930951,(float)20);
-
-
-
-
-
 
     srand (time(NULL));
     
     std::vector<Body>  bodies;
-    
-
 
     bodies.push_back( Body(1392684e3/2.,1.9891e30,vec3(0,0,0),vec3(0,0,0)));
     float theta = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/3.14));
@@ -89,7 +79,7 @@ int main(int argv,char** argc) {
     bodies.push_back( Body(6792.e3*GRAPHIC_SCALE/2.,0.642e24,(float)227.9e9*vec3(cos(theta),sin(theta),0),(float)24.1e3*vec3(cos(theta+M_PI/2),sin(theta+M_PI/2),0)));
     theta = theta = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/3.14));
     bodies.push_back( Body(142984e3*GRAPHIC_SCALE/2.,18982e24,(float)778.6e9*vec3(cos(theta),sin(theta),0),(float)13.1e3*vec3(cos(theta+M_PI/2),sin(theta+M_PI/2),0)));
-    freeCamera.moveFront(-1392684e3*100/UNITEASTRONOMIQUE);
+    camera.moveFront(-1392684e3*100/UNITEASTRONOMIQUE);
 
 
 
@@ -97,7 +87,7 @@ int main(int argv,char** argc) {
     // Application loop:
     glm::ivec2 prevMousePos = windowManager.getMousePosition();
     double PreviousT =0;
-
+    std::vector<glm::mat4> matrix ;
     bool done = false;
     while(!done) 
     {
@@ -117,9 +107,30 @@ int main(int argv,char** argc) {
                 
                 //camera.moveFront(10000);
             }
+            if(windowManager.isKeyPressed(SDLK_z))
+            {   
+                
+                //camera.moveFront(10000);
+            }
             if(windowManager.isKeyPressed(SDLK_s))
             {   
                // camera.moveFront(-1000);
+
+            }
+            if(windowManager.isKeyPressed(SDLK_c))
+            {   
+
+                if(camera.getType()=="FreeflyCamera")
+                {
+                    camera = Camera(std::unique_ptr<TrackballCamera>(new TrackballCamera()));
+                    camera.moveFront(-1392684e3*100/UNITEASTRONOMIQUE);
+
+                }
+                else if(camera.getType()=="TrackballCamera")
+                {
+                    camera = Camera(std::unique_ptr<FreeflyCamera>(new FreeflyCamera()));
+                    camera.moveFront(-1392684e3*100/UNITEASTRONOMIQUE);
+                }
 
             }
             if(windowManager.isKeyPressed(SDLK_q))
@@ -140,7 +151,7 @@ int main(int argv,char** argc) {
             if(windowManager.isKeyPressed(SDLK_LCTRL))
             {
                 glm::ivec2 mousePos = windowManager.getMousePosition();
-                freeCamera.moveFront((-mousePos[1]+prevMousePos[1])*0.0000930951);
+                camera.moveFront((-mousePos[1]+prevMousePos[1])*0.0000930951);
                 
             }
 
@@ -160,11 +171,22 @@ int main(int argv,char** argc) {
 
         for (int i = 0 ; i< bodies.size();i++)
         {
-            MVMatrix = bodies[i].viewMatrixBody(  &windowManager,freeCamera.getViewMatrix() );
+            if(camera.getType()=="TrackballCamera")
+            {
+                MVMatrix =bodies[i].viewMatrixBody(  &windowManager,camera.getViewMatrix(bodies[0].position,-1392684e3*100/UNITEASTRONOMIQUE,fov));
+              //  std::cout<<"hello"<<std::endl;
+            }
+            else{MVMatrix = bodies[i].viewMatrixBody(  &windowManager,camera.getViewMatrix() );
+            //std::cout<<"hello2"<<std::endl;
+            }
             NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
             engine.renderPlanet(MVMatrix,ProjMatrix,NormalMatrix);
             
-            std::vector<glm::mat4> matrix = bodies[i].viewMatrixTrail(  &windowManager,freeCamera.getViewMatrix() );
+            if(camera.getType()=="TrackballCamera")
+            {
+                matrix = bodies[i].viewMatrixTrail(  &windowManager,camera.getViewMatrix(bodies[0].position,-1392684e3*100/UNITEASTRONOMIQUE,fov));
+            }
+            else {matrix = bodies[i].viewMatrixTrail(  &windowManager,camera.getViewMatrix() );}
             for(int j = 0 ; j<matrix.size();j+=TRAIL_RENDER_FACTOR)
             {
                 NormalMatrix = glm::transpose(glm::inverse(matrix[j]));
