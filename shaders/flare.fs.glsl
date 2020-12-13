1cfd6e6c68fcd5f045afd78e48f1c32ec8c86f19
alstit <alexyls78@gmail.com>
2020-12-13 21:56:38 +0100

    fine tuning of flare

--- shaders/flare.fs.glsl
+++ shaders/flare.fs.glsl
@@ -4,6 +4,7 @@ precision mediump float;
 
 
 in vec3 localPosition;
+in vec4 globalPosition;
 out vec4 fFragColor;
 
 vec3 color(vec3 position)
@@ -13,11 +14,21 @@ vec3 color(vec3 position)
 	return vec3(valueR,valueG,0);
 }
 
+float intensity(vec4 aposition)
+{
+    float intensity = min(1.0f,pow(2.0f*distance(2.0*vec2(aposition.x,aposition.y),vec2(0,0)),10.0f));
+
+    //if(intensity<.5f){return 0.0f;}
+    
+    return intensity;
+}
+
+
 void main()
 {
     //fFragColor=vec4(1.f,1.f,0,0.5f);
     
    //fFragColor = vec4(1,0,0, 0);
-   fFragColor = vec4(color(localPosition),0);
+   fFragColor = intensity(globalPosition)*vec4(color(localPosition),0);
    //gl_FragColor = vec4(1.f,1.f,1.f,0.5f); 
 }
--- shaders/flare.vs.glsl
+++ shaders/flare.vs.glsl
@@ -9,9 +9,11 @@ uniform mat4 uMVMatrix;
 uniform mat4 uNormalMatrix;
 
 out vec3 localPosition;
+out vec4 globalPosition;
 
 void main()
 {
+    globalPosition = uMVMatrix*vec4(position,1.0);
 	localPosition=position;
     gl_Position=uMVMatrix*vec4(position,1.0);
 }
